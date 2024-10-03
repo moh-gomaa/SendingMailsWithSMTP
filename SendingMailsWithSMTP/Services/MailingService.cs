1d87a16b9e91624ab51ffe6370cb4fdbb76b401f
Mohaned Gomaa <mohamed.gomaa@intalio.com>
2024-10-03 14:02:05 +0300

    -Initialize .Net 8 API web app. -Write SMTP mail configuration in appsetting.json -Add model which contails mail settings. -Implement mailing service with its interface.

---
+++ SendingMailsWithSMTP/Services/MailingService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Options;
+using SendingMailsWithSMTP.Models;
+
+namespace SendingMailsWithSMTP.Services
+{
+    public class MailingService : IMailingService
+    {
+        private MailSettings _mailSettings;
+
+        public MailingService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+
+        public Task SendEmailAsync(string mailTo, string subject, string body, IList<IFormFile> attachments = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
